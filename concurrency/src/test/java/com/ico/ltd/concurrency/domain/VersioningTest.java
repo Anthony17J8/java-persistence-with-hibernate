8cf23785dfc05e66b4fc29afce9bfb8023818235
Anton Kiselev <j8kiselev@gmail.com>
2020-02-15 15:20:06 +0300

    Manual version checking

--- concurrency/src/test/java/com/ico/ltd/concurrency/domain/VersioningTest.java
+++ concurrency/src/test/java/com/ico/ltd/concurrency/domain/VersioningTest.java
@@ -1,6 +1,7 @@
 package com.ico.ltd.concurrency.domain;
 
 import com.ico.ltd.concurrency.config.PersistenceConfig;
+import com.ico.ltd.concurrency.util.TestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +12,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.LockModeType;
 import javax.persistence.OptimisticLockException;
+import javax.persistence.RollbackException;
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -87,5 +92,109 @@ class VersioningTest {
         } finally {
             tx.rollback();
         }
+    }
+
+    @Test
+    void manualVersionChecking() throws Exception {
+        ConcurrencyTestData testData = storeCategoriesAndItems();
+        Long[] CATEGORIES = testData.categories.identifiers;
+        em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            BigDecimal totalPrice = new BigDecimal(0);
+
+            for (Long catId : CATEGORIES) {
+                /*
+                   For each <code>Category</code>, query all <code>Item</code> instances with
+                   an <code>OPTIMISTIC</code> lock mode. Hibernate now knows it has to
+                   check each <code>Item</code> at flush time.
+                 */
+                List<Item> items = em
+                        .createQuery("select i from Item i where i.category.id =:catId", Item.class)
+                        .setLockMode(LockModeType.OPTIMISTIC)
+                        .setParameter("catId", catId)
+                        .getResultList();
+
+                for (Item item : items) {
+                    totalPrice = totalPrice.add(item.getBuyNowPrice());
+                }
+
+                // Now a concurrent transaction will move an item to another category
+                if (catId.equals(testData.categories.getFirstId())) {
+                    Executors.newSingleThreadExecutor().submit(() -> {
+                        EntityManager sEm = emf.createEntityManager();
+                        EntityTransaction sTx = sEm.getTransaction();
+
+                        try {
+                            sTx.begin();
+                            // Moving the first item from the first category into the last category
+                            List<Item> list = sEm.createQuery("select i from Item i where  i.category.id =:catId", Item.class)
+                                    .setParameter("catId", testData.categories.getFirstId())
+                                    .getResultList();
+
+                            Category lastCat = sEm.getReference(Category.class, testData.categories.getLastId());
+                            list.iterator().next().setCategory(lastCat);
+
+                            sTx.commit();
+                            sEm.close();
+
+                        } catch (Exception exc) {
+                            System.out.println("Move item to another category failed");
+                            exc.printStackTrace();
+                        }
+                    }).get();
+                }
+            }
+
+           /*
+               For each <code>Item</code> loaded earlier with the locking query, Hibernate will
+               now execute a <code>SELECT</code> during flushing. It checks if the database
+               version of each <code>ITEM</code> row is still the same as when it was loaded
+               earlier. If any <code>ITEM</code> row has a different version, or the row doesn't
+               exist anymore, an <code>OptimisticLockException</code> will be thrown.
+             */
+            assertThrows(RollbackException.class, tx::commit);
+            em.close();
+
+//            assertEquals(totalPrice.toString(), "108.00");
+        } finally {
+            tx.rollback();
+        }
+
+    }
+
+    private ConcurrencyTestData storeCategoriesAndItems() throws Exception {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        ConcurrencyTestData testData = new ConcurrencyTestData();
+        testData.categories = new TestData(new Long[3]);
+        testData.items = new TestData(new Long[5]);
+        for (int i = 1; i <= testData.categories.identifiers.length; i++) {
+            Category category = new Category();
+            category.setName("Category " + i);
+
+            em.persist(category);
+            testData.categories.identifiers[i - 1] = category.getId();
+
+            for (int j = 1; j <= testData.categories.identifiers.length; j++) {
+                Item item = new Item("Item " + j);
+                item.setCategory(category);
+                item.setBuyNowPrice(new BigDecimal(10 + j));
+                em.persist(item);
+                testData.items.identifiers[(i - 1) + (j - 1)] = item.getId(); // 0 1 2 // 1 2 3 //
+            }
+        }
+        tx.commit();
+        em.close();
+        return testData;
+    }
+
+    private static class ConcurrencyTestData {
+
+        private TestData categories;
+
+        private TestData items;
     }
 }
