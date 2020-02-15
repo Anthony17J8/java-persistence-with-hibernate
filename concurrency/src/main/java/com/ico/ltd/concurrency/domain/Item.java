04f7565981fb33fb7f5a06aa169a790a81df5e8e
Anton Kiselev <j8kiselev@gmail.com>
2020-02-15 10:19:07 +0300

    Versioning with timestamps

---
+++ concurrency/src/main/java/com/ico/ltd/concurrency/domain/Item.java
@@ -0,0 +1,44 @@
+package com.ico.ltd.concurrency.domain;
+
+import com.ico.ltd.concurrency.Constants;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    protected Long id;
+
+    @Version
+    // Optional: @org.hibernate.annotations.Type(type = "dbtimestamp")
+    protected Date lastUpdated;
+
+    @NotNull
+    protected String name;
+
+    public Item() {
+    }
+
+    public Long getId() { // Optional but useful
+        return id;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
