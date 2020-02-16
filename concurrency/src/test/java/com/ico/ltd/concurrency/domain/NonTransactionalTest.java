060776c3baa37cf89195430daa692b470f15923e
Anton Kiselev <j8kiselev@gmail.com>
2020-02-16 16:31:50 +0300

    Queuing data modifications in unsynchronized persistence context

--- concurrency/src/test/java/com/ico/ltd/concurrency/domain/NonTransactionalTest.java
+++ concurrency/src/test/java/com/ico/ltd/concurrency/domain/NonTransactionalTest.java
@@ -1,6 +1,7 @@
 package com.ico.ltd.concurrency.domain;
 
 import com.ico.ltd.concurrency.config.PersistenceConfig;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = PersistenceConfig.class)
@@ -102,6 +104,101 @@ public class NonTransactionalTest {
             assertEquals(item.getName(), "Original Name");
 
             em.close();
+        }
+    }
+
+    @Disabled("Need to set up Jta Transaction Manager")
+    @Test
+    void queuingDataModifications() throws Exception {
+        Long ITEM_ID;
+        {
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            // save Entity
+            Item someItem = new Item("Some Name");
+
+            /*
+               You can call <code>persist()</code> to save a transient entity instance with an
+               unsynchronized persistence context. Hibernate will only fetch a new identifier
+               value, typically by calling a database sequence, and assign it to the instance.
+               The instance is now in persistent state in the context but the SQL
+               <code>INSERT</code> hasn't happened. Note that this is only possible with
+               <em>pre-insert</em> identifier generators; see <a href="#GeneratorStrategies"/>.
+            */
+            em.persist(someItem);
+
+            assertNotNull(someItem.getId());
+            ITEM_ID = someItem.getId();
+
+            /*
+               When you are ready to store the changes, join the persistence context with
+               a transaction. Synchronization and flushing will occur as usual, when the
+               transaction commits. Hibernate writes all queued operations to the database.
+             */
+            tx.begin();
+            if (!em.isJoinedToTransaction()) {
+                em.joinTransaction();
+            }
+            tx.commit();
+            em.close();
+        }
+
+        checkStateEntity(ITEM_ID, "Some Name");
+
+        // update Entity
+        {
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+
+            Item item = em.find(Item.class, ITEM_ID);
+            em.close();
+
+            item.setName("New Name");
+            Item merged = em.merge(item);
+
+            tx.begin();
+            em.joinTransaction();
+            tx.commit(); // Flush!
+            em.close();
+        }
+
+        checkStateEntity(ITEM_ID, "New Name");
+
+        // delete Entity
+        {
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+
+            Item item = em.find(Item.class, ITEM_ID);
+            em.remove(item);
+
+            tx.begin();
+            em.joinTransaction();
+            tx.commit();
+            em.close();
+        }
+
+        // check count items
+        {
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            assertEquals(em.createQuery("select count(i) from Item i").getSingleResult(), 0);
+            tx.commit();
+            em.close();
+        }
+    }
+
+    private void checkStateEntity(Long ITEM_ID, String name) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Item item = em.find(Item.class, ITEM_ID);
+            assertEquals(name, item.getName());
+            tx.commit();
+        } finally {
+            tx.rollback();
         }
     }
 }
