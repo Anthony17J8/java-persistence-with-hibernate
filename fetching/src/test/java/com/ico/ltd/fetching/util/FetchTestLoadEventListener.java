2dbafaaf31aa9f1e7985d9e7ba77765b0c9af1fc
Anton Kiselev <j8kiselev@gmail.com>
2020-02-23 10:59:59 +0300

    Set up FetchTestLoadEventListener

---
+++ fetching/src/test/java/com/ico/ltd/fetching/util/FetchTestLoadEventListener.java
@@ -0,0 +1,42 @@
+package com.ico.ltd.fetching.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.event.spi.PostLoadEvent;
+import org.hibernate.event.spi.PostLoadEventListener;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Useful when testing how much data was loaded by Hibernate, counts
+ * the loaded entity instances by type.
+ */
+public class FetchTestLoadEventListener implements PostLoadEventListener {
+
+    protected Map<Class, Integer> loadCount = new HashMap<>();
+
+    public FetchTestLoadEventListener(EntityManagerFactory emf) {
+        ServiceRegistry serviceRegistry = ((SessionFactoryImplementor) emf.unwrap(SessionFactory.class)).getServiceRegistry();
+        serviceRegistry.getService(EventListenerRegistry.class)
+                .appendListeners(EventType.POST_LOAD, this);
+    }
+
+    @Override
+    public void onPostLoad(PostLoadEvent event) {
+        Class entityType = event.getEntity().getClass();
+        loadCount.computeIfPresent(entityType, (k, v) -> v + 1);
+    }
+
+    public int getLoadCount(Class entityType) {
+        return loadCount.getOrDefault(entityType, 0);
+    }
+
+    public void reset() {
+        loadCount.clear();
+    }
+}
