b955804948560d761f0f59a4b2a7546c3012cefd
Anton Kiselev <j8kiselev@gmail.com>
2020-03-01 19:47:17 +0300

    Create Typed queries

--- querying/src/test/java/com/ico/ltd/querying/domain/CreateExecuteQueries.java
+++ querying/src/test/java/com/ico/ltd/querying/domain/CreateExecuteQueries.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -43,6 +45,52 @@ public class CreateExecuteQueries extends QueryingTest {
                 );
 
                 assertEquals(query.getResultList().size(), 3);
+            }
+
+            tx.commit();
+            em.close();
+        } finally {
+            tx.rollback();
+        }
+    }
+
+    @Test
+    void createTypedQueries() throws Exception {
+        TestDataCategoriesItems testData = storeTestData();
+        Long ITEM_ID = testData.items.getFirstId();
+
+        em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            {
+                Query query = em.createQuery(
+                        "select i from Item i where i.id = :id"
+                ).setParameter("id", ITEM_ID);
+
+                Item result = (Item) query.getSingleResult();
+
+                assertEquals(result.getId(), ITEM_ID);
+            }
+
+            {
+                TypedQuery<Item> query = em.createQuery("SELECT i FROM Item i WHERE i.id = :id", Item.class)
+                        .setParameter("id", ITEM_ID);
+
+                Item result = query.getSingleResult(); // No cast needed!
+
+                assertEquals(ITEM_ID, result.getId());
+            }
+
+            {
+                CriteriaBuilder cb = em.getCriteriaBuilder();
+                CriteriaQuery<Item> criteria = cb.createQuery(Item.class);
+                Root<Item> root = criteria.from(Item.class);
+                criteria.select(root).where(cb.equal(root.get("id"), ITEM_ID));
+
+                TypedQuery<Item> query = em.createQuery(criteria);
+                Item item = query.getSingleResult();
+                assertEquals(ITEM_ID, item.getId()); // No cast needed!
             }
 
             tx.commit();
